5d85d8ef9ebf01dd9266a41c0bc3ed9da3829d85
GeoffreyGone <geoffreygone@gmail.com>
2020-09-14 06:13:45 +0000

    covid app

---
+++ build/precache-manifest.f3a3b2a49ea283b05d38e1e4aaf201e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "355d714b6276f2fc1a157f0be0245d81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c48dd04b062c317c72d",
+    "url": "/static/css/main.0faaeeda.chunk.css"
+  },
+  {
+    "revision": "c8550a42f4e035516de8",
+    "url": "/static/js/2.a9857d7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a9857d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c48dd04b062c317c72d",
+    "url": "/static/js/main.e311510b.chunk.js"
+  },
+  {
+    "revision": "89ac118f8d664424e2aa",
+    "url": "/static/js/runtime-main.40fd9fda.js"
+  }
+]);
